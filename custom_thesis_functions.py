cae3722d5155aaa9116d878469f9c0c74279e184
Jannik Carl <jannik.carl@posteo.de>
2018-09-12 15:32:37 +0200

    additional graphs for validation

--- custom_thesis_functions.py
+++ custom_thesis_functions.py
@@ -1,2 +1,5 @@
+path_import_data = r"C:\Users\janni\OneDrive\TU Berlin\4 - Abschlussarbeiten\01 - Bachelorarbeit\modell\data\\"
+path_pickle_data = r"C:\Users\janni\OneDrive\TU Berlin\4 - Abschlussarbeiten\01 - Bachelorarbeit\validierung\\"
+
 def create_date_format(period):
     return "({0}) ({1})".format(period[0].strftime("%Y-%m-%d %H-%M"), period[-1].strftime("%Y-%m-%d %H-%M"))
