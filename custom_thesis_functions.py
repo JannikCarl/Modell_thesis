ea3ba892b7743186ca17b55ff2e94e81cf6a0ec5
Jannik Carl <jannik.carl@posteo.de>
2018-09-12 14:25:15 +0200

    Added autocreation of folders and saving of results as png

--- custom_thesis_functions.py
+++ custom_thesis_functions.py
@@ -1,4 +1,2 @@
-def create_pickle_path(contentstring, period, pickle_loc):
-    date_stamp = "({0}) ({1})".format(period[0].strftime("%Y-%m-%d %H-%M"),
-                                  period[-1].strftime("%Y-%m-%d %H-%M"))
-    return pickle_loc + contentstring + " " + date_stamp + ".pickle"
+def create_date_format(period):
+    return "({0}) ({1})".format(period[0].strftime("%Y-%m-%d %H-%M"), period[-1].strftime("%Y-%m-%d %H-%M"))
