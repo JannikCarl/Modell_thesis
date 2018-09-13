b5734f3f21d6eb7aa3848b50e0f93106e81c2138
Jannik Carl <jannik.carl@posteo.de>
2018-09-13 16:11:07 +0200

    Updated carrier prices

--- custom_thesis_functions.py
+++ custom_thesis_functions.py
@@ -2,4 +2,5 @@ path_import_data = r"C:\Users\janni\OneDrive\TU Berlin\4 - Abschlussarbeiten\01 
 path_pickle_data = r"C:\Users\janni\OneDrive\TU Berlin\4 - Abschlussarbeiten\01 - Bachelorarbeit\validierung\\"
 
 def create_date_format(period):
+    """Put first and last timestamp of given period in brackets"""
     return "({0}) ({1})".format(period[0].strftime("%Y-%m-%d %H-%M"), period[-1].strftime("%Y-%m-%d %H-%M"))
